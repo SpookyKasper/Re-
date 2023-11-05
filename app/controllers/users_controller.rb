7c5208813f414e8e824a3f3a61bea710bad8d569
Kasper <kasperbak89@pm.me>
2023-11-05 21:07:36 +0100

    Integrate the editing of the User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,27 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
+    @user.username = 'YourName'
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    # @user = User.new(user_params)
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
     if @user.save
-      flash[:success] = "Great! Your post has been created!"
       redirect_to new_user_path
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password, :id)
   end
 end
