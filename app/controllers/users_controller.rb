556c6956ea450e4d07a98d50b150349da7fcadf9
Kasper <kasperbak89@pm.me>
2023-11-06 12:04:26 +0100

    Add form template and modify new and edit to work with it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to user_path(@user)
+      redirect_to @user
     else
       render :new, status: :unprocessable_entity
     end
@@ -16,6 +16,16 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
